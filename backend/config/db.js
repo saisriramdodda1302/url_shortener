3d050976dccd573188e473c20f52405f89636fb2
Sai Sriram Dodda <saisriramdodda1302@gmail.com>
2026-06-14 23:21:51 +0530

    databases in my proj.

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = new Pool({
+  user: process.env.DB_USER || 'myuser',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'urldb',
+  password: process.env.DB_PASSWORD || 'mypassword',
+  port: process.env.DB_PORT || 5432,
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
---
+++ backend/config/redis.js
@@ -0,0 +1,12 @@
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+// connects to Redis immediately.
+await redisClient.connect().catch(console.error);
